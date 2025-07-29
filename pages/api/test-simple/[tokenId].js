3a42720c5552b110233aa55d1793c08172627e14
ADRIAN <adriangallery@adriancerda.com>
2025-07-29 13:26:55 +0200

    feat: Añadir trait animado 600.svg al test-simple - Subir 600.svg animado a GitHub - Añadir trait en capa superior del SVG - Incluir animación con dos frames alternados - Actualizar logs de debug para mostrar nueva capa

--- pages/api/test-simple/[tokenId].js
+++ pages/api/test-simple/[tokenId].js
@@ -275,6 +275,9 @@ export default async function handler(req, res) {
         <!-- Imagen del trait (centrada en el contenedor) usando <image> -->
         <image x="84" y="120" width="600" height="600" href="${traitImageData}" />
         
+        <!-- TRAIT ANIMADO 600.svg en capa superior -->
+        <image x="84" y="120" width="600" height="600" href="data:image/svg+xml;base64,${Buffer.from(fs.readFileSync(path.join(process.cwd(), 'public', 'labimages', '600.svg'), 'utf8')).toString('base64')}" />
+        
         <!-- Tag de rareza (superior izquierda) - convertido a path -->
         ${textToSVGElement(rarity.tag, {
           x: 84 + 10,
@@ -351,11 +354,12 @@ export default async function handler(req, res) {
     console.log(`[test-simple] DEBUG - 3. Contenedor con fondo dinámico (${rarity.bg}20)`);
     console.log(`[test-simple] DEBUG - 4. Mannequin (base del personaje) - MÉTODO PERSONALIZADO`);
     console.log(`[test-simple] DEBUG - 5. Trait ${cleanTokenId} (encima del mannequin) - MÉTODO PERSONALIZADO`);
-    console.log(`[test-simple] DEBUG - 6. Tag de rareza: ${rarity.tag}`);
-    console.log(`[test-simple] DEBUG - 7. Nombre: ${tokenData.name}`);
-    console.log(`[test-simple] DEBUG - 8. Datos: ${tokenData.category}, ${totalMinted}, ${tokenData.floppy || 'OG'}`);
-    console.log(`[test-simple] DEBUG - 9. Logo AdrianLAB`);
-    console.log(`[test-simple] DEBUG - 10. Indicador de test GIF`);
+    console.log(`[test-simple] DEBUG - 6. TRAIT ANIMADO 600.svg`);
+    console.log(`[test-simple] DEBUG - 7. Tag de rareza: ${rarity.tag}`);
+    console.log(`[test-simple] DEBUG - 8. Nombre: ${tokenData.name}`);
+    console.log(`[test-simple] DEBUG - 9. Datos: ${tokenData.category}, ${totalMinted}, ${tokenData.floppy || 'OG'}`);
+    console.log(`[test-simple] DEBUG - 10. Logo AdrianLAB`);
+    console.log(`[test-simple] DEBUG - 11. Indicador de test GIF`);
 
     try {
       // Generar GIF animado
