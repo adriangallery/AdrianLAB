daf248ff327c941ac168d0c0b24f04f4469eb52e
ADRIAN <adriangallery@adriancerda.com>
2026-01-07 21:14:36 +0100

    Actualizar animation_url del token 682 a mcinteractive

--- pages/api/metadata/[tokenId].js
+++ pages/api/metadata/[tokenId].js
@@ -952,7 +952,7 @@ export default async function handler(req, res) {
     // Este es un test temporal para añadir animation_url al token 682
     // Para revertir: simplemente eliminar este bloque if completo
     if (tokenIdNum === 682) {
-      baseMetadata.animation_url = 'https://adrianzero.com/cigarrettev3/';
+      baseMetadata.animation_url = 'https://adrianzero.com/mcinteractive/';
       console.log(`[metadata] 🧪 TEST: animation_url añadido para token 682 → ${baseMetadata.animation_url}`);
     }
     // ===== FIN PRUEBA PUNTUAL =====
